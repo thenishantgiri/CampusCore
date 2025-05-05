38a98f99abdbbe5098f37728f01b84a5f9fd2c53
thenishantgiri <nishantkrgiri@gmail.com>
2025-05-05 09:50:36 +0530

    feat(auth): protect routes using JwtAuthGuard and GetUser decorator
    - Added JwtAuthGuard to secure endpoints with JWT
    - Implemented GetUser decorator to access authenticated user
    - Updated JwtStrategy to return full SafeUser from Prisma
    - Secured GET /auth/me to return authenticated user details
    - Fixed ESLint type safety issues in auth flow

--- apps/erp-backend/src/auth/auth.controller.ts
+++ apps/erp-backend/src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -9,6 +11,8 @@ import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { SafeUser } from './types/safe-user.interface';
 import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GetUser } from './decorators/get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -26,5 +30,11 @@ export class AuthController {
     @Body() dto: LoginDto,
   ): Promise<{ accessToken: string; user: SafeUser }> {
     return this.authService.login(dto);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@GetUser() user: SafeUser): SafeUser {
+    return user;
   }
 }
--- apps/erp-backend/src/auth/strategies/jwt.strategy.ts
+++ apps/erp-backend/src/auth/strategies/jwt.strategy.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from '../types/jwt-payload.interface';
+import { PrismaService } from '../../prisma/prisma.service';
+import { SafeUser } from '../types/safe-user.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private prisma: PrismaService) {
     const jwtSecret = process.env.JWT_SECRET;
     if (!jwtSecret)
       throw new Error('JWT_SECRET is not defined in environment variables.');
@@ -17,7 +19,22 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: JwtPayload) {
-    return { userId: payload.sub, email: payload.email };
+  async validate(payload: JwtPayload): Promise<SafeUser> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        roleId: true,
+        createdAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 }
